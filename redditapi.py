225f22cb9452d66dcc4644048d7fbb77c2c823e2
chris7neves <christopher.neves@hotmail.ca>
2020-08-27 20:55:25 -0400

    Added regex terms to clean text extracted with API.

--- redditapi.py
+++ redditapi.py
@@ -1,16 +1,52 @@
 import praw
 import config
+import re
+import maps
 
+def expand_contractions(word, contraction_map=maps.CONTRACTION_MAP):
+    pass
 
-def GetTitleWordsTop(subreddit):
+def clean_text(text):
+    # Regex terms
+    tag_re = re.compile(r'^\[.*?\]', re.IGNORECASE)
+    ytlink_re = re.compile(r'\[(http).*?\]', re.IGNORECASE)
+    symbol_re = re.compile(r'[\.\^\$\*\+\?\{\}\[\]\\\|\(\)\-\@\:]', re.IGNORECASE)
+
+    cleaned = text
+    mo = tag_re.search(cleaned)
+    if mo:
+        cleaned = cleaned.replace(mo.group(), '')
+    mo = ytlink_re.search(cleaned)
+    if mo:
+        cleaned = cleaned.replace(mo.group(), '')
+    mo = symbol_re.findall(cleaned)
+    for symbol in mo:
+        cleaned = cleaned.replace(symbol, '')
+
+    # Expand contractions
+    # maybe get rid of possessives
+    return cleaned
+
+def get_titlewords_topalltime(subreddit):
     """
     Accepts a subreddit and returns the word occurances of the words found in the titles
     of the first 1000 posts when sorted by Top.
     """
     r = praw.Reddit(user_agent=config.user_agent, client_secret=config.client_secret, client_id=config.client_id)
-    sub = r.get_subreddit(subreddit)
-    word_occurances = []
-    return word_occurances
+    word_occurances = {}
+    
+    for submission in r.subreddit(subreddit).top(limit=100):
+        title = clean_text(submission.title)
+        # tokenize
+        # add to dict if key doesnt exist, if it does, increment frequency
+
+
+
+        print(title.strip())
+    # return word_occurances
+
+
+get_titlewords_topalltime("machinelearning")
 
 #TODO: Implement checkboxes on the front end so the user can choose to sort by new, top, hot etc
 #TODO: allow the user to select comments or titles for the posts
